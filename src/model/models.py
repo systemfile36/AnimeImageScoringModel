6d09217145c626659843ba8233462ecf2049d31b
systemfile36 <systemfile36@gmail.com>
2025-05-13 01:55:14 +0900

    feat(models.py), fix: Add CNN based trasformer model
    Add meta multi-task model with cnn backbone + transformer block.
    Fix the code where stacking TransformerBlock.
    Refactor positional embedding for score prediction.

--- src/model/models.py
+++ src/model/models.py
@@ -3,7 +3,7 @@ from tensorflow.keras import layers, Model, Input
 from typing import Callable
 
 from src.utils import get_filename_based_logger
-from src.model.layers import TransformerBlock, AddCLSandPositional
+from src.model.layers import TransformerBlock, AddCLSandPositional, AddPositionalOnly
 from src.model.layers import data_augmentation
 
 logger = get_filename_based_logger(__file__)
@@ -180,22 +180,29 @@ def create_vit_meta_multitask_reg_model(
 
     # 3. Positional embedding for score prediction
 
-    # Get Token length 
-    token_sequence_length = token_sequence.shape[1]
+    # # Get Token length 
+    # token_sequence_length = token_sequence.shape[1]
 
-    # Positional embedding for score prediction. 
-    score_pos_embedding = layers.Embedding(input_dim=512, output_dim=token_dim, name="score_pos_embedding")
-    # Get position tensor. length is equal to length of token sequence
-    score_positions = tf.range(start=0, limit=token_sequence_length, delta=1)
-    token_sequence += score_pos_embedding(score_positions)
+    # # Positional embedding for score prediction. 
+    # score_pos_embedding = layers.Embedding(input_dim=512, output_dim=token_dim, name="score_pos_embedding")
+    # # Get position tensor. length is equal to length of token sequence
+    # score_positions = tf.range(start=0, limit=token_sequence_length, delta=1)
+    # token_sequence += score_pos_embedding(score_positions)
+
+    token_sequence = AddPositionalOnly(
+        num_patches=patch_tokens.shape[1] + meta_tokens.shape[1],
+        projection_dim=token_dim,
+        name_prefix="score"
+    )(token_sequence)
 
     # 4. Transformer block for score prediction 
+    x = token_sequence
     for i in range(transformer_count):
         x = TransformerBlock(
             projection_dim=token_dim, 
             ffn_dim=final_ff_dim,
             num_heads=4, dropout_rate=0.1, block_index=i, name_prefix="score"
-        )(token_sequence)
+        )(x)
 
     pooled = layers.GlobalAveragePooling1D(name="score_pooling")(x)
     x = layers.Dense(final_ff_dim, activation='relu')(pooled)
@@ -206,6 +213,112 @@ def create_vit_meta_multitask_reg_model(
     model = Model(inputs=input, outputs=[
         ai_logits, rating_logits, score_pred
     ])
+
+    return model
+
+def create_cnn_meta_multitask_transformer_reg_model(
+        input: Input, cnn_delegate: Callable[..., any], 
+        token_dim = 768, final_ff_dim=2048, dropout_rate=0.1, transformer_count=2, 
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.2,
+        **kwargs
+) -> Model:
+    """
+    Create multi-task model with metadata, 'ai_prediction', 'rating_prediction' 
+    based given CNN backbone.
+
+    `cnn_delegate` must return 'feature map'. DONT add pooling layer.
+
+    Dataset shape must be 
+
+    ```
+    (image, {
+        'ai_prediction': "(1,), float32, 0 or 1", 
+        'rating_prediction': "(3,), float32, one hot vector", 
+        'score_prediction': "float32"
+    })
+    ```
+
+    """
+
+    # 1. Convert cnn feature map to token sequence.
+
+    # Add augmentation layers when flag set
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        cnn_feature = cnn_delegate(x, **kwargs)
+    else:
+        cnn_feature = cnn_delegate(input, **kwargs)
+
+    # Flatten to tokens
+    # (batch, H, W, C) -> (batch, H * W, C)
+    # auto calculate by `-1`
+    patch_tokens = layers.Reshape((-1, cnn_feature.shape[-1]), name="flatten_cnn_tokens")(cnn_feature)
+
+    # Project token to `token_dim`
+    # Add activation
+    patch_tokens = layers.Dense(token_dim * 2, name="cnn_patch_proj_dense_1")(patch_tokens)
+    patch_tokens = layers.Activation("relu", name="cnn_patch_proj_relu")(patch_tokens)
+    patch_tokens = layers.Dense(token_dim, name="cnn_patch_proj_dense_2")(patch_tokens)
+
+    # Flatten cnn feature map by GAP for meta prediction
+    global_feature = layers.GlobalAveragePooling2D(name="global_feature")(cnn_feature)
+
+    # Predict metadata
+    # Make sure output is tf.float32 for mixed precision
+    ai_logits = layers.Dense(1, activation="sigmoid", dtype=tf.float32, name="ai_prediction")(global_feature)
+    rating_logits = layers.Dense(3, activation="softmax", dtype=tf.float32, name="rating_prediction")(global_feature)
+
+    # Embedding meta
+    ai_token = layers.Dense(token_dim, activation="relu", name="ai_token_proj")(ai_logits)
+    rating_token = layers.Dense(token_dim, activation="relu", name="rating_token_proj")(rating_logits)
+    
+    # Wrapping tf function to keras.layers.Layer class
+    class Stack(layers.Layer):
+        def call(self, x):
+            return tf.stack(x, axis=1)
+
+    meta_tokens = Stack()([ai_token, rating_token])  #shape = (batch, 2, token_dim)
+
+    # Concatenate cnn feature token and meta tokens
+    token_sequence = layers.Concatenate(axis=1, name="concat_tokens")([patch_tokens, meta_tokens])
+
+    # # Get Token length 
+    # token_sequence_length = token_sequence.shape[1]
+
+    # # Positional embedding for score prediction. 
+    # score_pos_embedding = layers.Embedding(input_dim=512, output_dim=token_dim, name="score_pos_embedding")
+    # # Get position tensor. length is equal to length of token sequence
+    # score_positions = tf.range(start=0, limit=token_sequence_length, delta=1)
+    # token_sequence += score_pos_embedding(score_positions)
+
+    token_sequence = AddPositionalOnly(
+        num_patches=patch_tokens.shape[1] + meta_tokens.shape[1],
+        projection_dim=token_dim,
+        name_prefix="score"
+    )(token_sequence)
+
+    x = token_sequence
+    # 4. Transformer block for score prediction 
+    for i in range(transformer_count):
+        x = TransformerBlock(
+            projection_dim=token_dim, 
+            ffn_dim=final_ff_dim,
+            num_heads=4, dropout_rate=0.1, block_index=i, name_prefix="score"
+        )(x)
+
+    # Pooling with GAP1D
+    pooled = layers.GlobalAveragePooling1D(name="score_pooling")(x)
+    x = layers.Dense(final_ff_dim, activation='relu')(pooled)
+    x = layers.Dropout(dropout_rate)(x)
+    
+    # Make sure output is tf.float32 for mixed precision
+    score_pred = layers.Dense(1, activation='linear', dtype=tf.float32, name="score_prediction")(x)
+
+    model = Model(inputs=input, outputs=[
+        ai_logits, rating_logits, score_pred
+    ])   
 
     return model
 
@@ -277,22 +390,29 @@ def create_vit_meta_tag_character_multitask_reg_model(
 
     # 3. Positional embedding for score prediction
 
-    # Get Token length 
-    token_sequence_length = token_sequence.shape[1]
+    # # Get Token length 
+    # token_sequence_length = token_sequence.shape[1]
 
-    # Positional embedding for score prediction. 
-    score_pos_embedding = layers.Embedding(input_dim=512, output_dim=token_dim, name="score_pos_embedding")
-    # Get position tensor. length is equal to length of token sequence
-    score_positions = tf.range(start=0, limit=token_sequence_length, delta=1)
-    token_sequence += score_pos_embedding(score_positions)
+    # # Positional embedding for score prediction. 
+    # score_pos_embedding = layers.Embedding(input_dim=512, output_dim=token_dim, name="score_pos_embedding")
+    # # Get position tensor. length is equal to length of token sequence
+    # score_positions = tf.range(start=0, limit=token_sequence_length, delta=1)
+    # token_sequence += score_pos_embedding(score_positions)
+
+    token_sequence = AddPositionalOnly(
+        num_patches=patch_tokens.shape[1] + meta_tokens.shape[1],
+        projection_dim=token_dim,
+        name_prefix="score"
+    )(token_sequence)
 
     # 4. Transformer block for score prediction 
+    x = token_sequence
     for i in range(transformer_count):
         x = TransformerBlock(
             projection_dim=token_dim, 
             ffn_dim=final_ff_dim,
             num_heads=4, dropout_rate=0.1, block_index=i, name_prefix="score"
-        )(token_sequence)
+        )(x)
 
     pooled = layers.GlobalAveragePooling1D(name="score_pooling")(x)
     x = layers.Dense(final_ff_dim, activation='relu')(pooled)
