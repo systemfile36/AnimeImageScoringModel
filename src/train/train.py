dfdfae2cb8e654b6f7ae5f4d53ea89bc07ad7434
systemfile36 <systemfile36@gmail.com>
2025-05-26 01:10:08 +0900

    feat(train.py): Add functions for train CNN+Transformer AI classification model
    Add function for train CNN + Transformer model for AI classification.
    Add custom LearningRateSchedule for warm-up + cosine decay.
    Add function for fine tuning with AdamW optimizer.

--- src/train/train.py
+++ src/train/train.py
@@ -37,6 +37,7 @@ from src.model import create_cnn_meta_multitask_transformer_reg_model
 from src.model import create_cnn_score_classification_model
 from src.model import create_cnn_rating_classification_model
 from src.model import create_cnn_ai_classification_model
+from src.model import create_cnn_transformer_ai_classification_model
 
 logger = get_filename_based_logger(__file__)
 
@@ -1092,6 +1093,185 @@ def train_ai_classification_model(
         new_learning_rate=1e-5
     )
 
+def train_ai_classification_transformer_model(
+    root_path: str, db_path: str, aliases_path: str, project_suffix:str, config_path: str, config_dict: dict    
+):
+    
+    project_setting = get_project_setting(root_path, db_path, aliases_path, project_suffix, config_path, config_dict)
+
+    config = project_setting['config']
+    db_path = project_setting['db_path']
+    aliases_path = project_setting['aliases_path']
+    project_path = project_setting['project_path']
+    width = project_setting['width']
+    height = project_setting['height']
+    learning_rate = project_setting['learning_rate']
+    batch_size = project_setting['batch_size']
+    epoch_count = project_setting['epoch_count']
+    loss_weights = project_setting['loss_weights']
+    data_augmentation = project_setting['data_augmentation']
+
+    logger.debug(f"Config set : {config}")
+
+    with open(project_setting['config_save_path'], "w", encoding="utf-8") as fs:
+        json.dump(config, fs, indent=4)
+
+        logger.info(f"Load records from {db_path}")
+
+    # Load records
+    records = load_records(root_path, db_path)
+
+    # Filter image file not exists
+    records = filter_image_exists(records)
+
+    # Split train and test
+    train_records, test_records = train_test_split(
+        records, test_size=0.3, random_state=42
+    )
+
+    # Logging for debug
+    logging_records(test_records, 'image_path', 'ai_prediction')
+
+    # Export test records for debug and statistics
+    test_records.to_csv(os.path.join(project_path, "test_records.csv"))
+
+    train_dataset = DatasetWrapperForAiClassification(
+        train_records, width=width, height=height,
+        normalize=False # normalize false for pre-trained EfficientNetB4
+    ).get_dataset(batch_size=batch_size)
+
+    test_dataset = DatasetWrapperForAiClassification(
+        test_records, width=width, height=height,
+        normalize=False # normalize false for pre-trained EfficientNetB4
+    ).get_dataset(batch_size=batch_size)
+
+    # Create Input layer
+    inputs = Input(shape=(height, width, 3), dtype=tf.float32, name="image_input")
+
+    augmentation: bool = data_augmentation is not None
+
+    model = create_cnn_transformer_ai_classification_model(
+        inputs,
+        src.model.cnn.create_efficientNet_b4_pretrained,
+        token_dim=768, final_ff_dim=2048, dropout_rate=0.1,
+        transformer_count=2,
+        augmentation=augmentation,
+        zoom_range=data_augmentation['zoom_range'],
+        rotation_range=data_augmentation['rotation_range'],
+        trainable=False,
+        pooling=True
+    )
+
+    model.summary()
+
+    model_json = model.to_json(indent=2)
+
+    with open(project_setting['model_json_path'], "w", encoding='utf-8') as fs:
+        fs.write(model_json)
+
+    csv_log_path = project_setting['csv_log_path']
+
+    # Create checkpoint directory
+    checkpoint_path = project_setting['checkpoint_path']
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+
+    callbacks = [
+        # Set EarlyStopping. 
+        # Monitor loss. stop training when no improvement in 5 epochs.
+        EarlyStopping(monitor="val_loss", mode="min", patience=5, restore_best_weights=True, verbose=1), 
+
+        # Save model for each epoch
+        ModelCheckpoint(filepath=os.path.join(checkpoint_path, "{epoch:02d}-{val_loss:.2f}.keras"),
+                        monitor="val_loss", mode="min", save_weights_only=False, verbose=1),
+        
+        CSVLogger(csv_log_path),
+		
+		# Terminate when loss or metric is NaN for safety
+		TerminateOnNaN()
+    ]
+
+    # Get steps per epoch (length of dataset / batch size)
+    steps_per_epoch = int(len(train_records['image_path']) / batch_size)
+
+    # Calculate count of total steps
+    total_steps = epoch_count * steps_per_epoch
+
+    # Set warm-up steps. 10% of total steps
+    warmup_steps = int(0.1 * total_steps)
+
+    # Set LearningRateSchedule
+    learning_rate_schedule = WarmUpCosineDecay(
+        base_lr=learning_rate,
+        total_steps=total_steps,
+        warmup_steps=warmup_steps
+    )
+
+    logger.debug(f"total_steps: {total_steps}, warmup_steps: {warmup_steps}")
+
+    model.compile(
+        # Use optimizer 'AdamW' for Transformer
+        # Set weight decay to avoid overfitting
+        optimizer=tf.keras.optimizers.AdamW(
+            learning_rate=learning_rate_schedule,
+            weight_decay=1e-4), 
+        # Set loss and metrics for binary classification
+        loss={
+            "ai_prediction": "binary_crossentropy"
+        }, 
+        metrics={
+            "ai_prediction": [
+                tf.keras.metrics.BinaryAccuracy(name="ai_acc"),
+                tf.keras.metrics.Precision(name="ai_precision"),
+                tf.keras.metrics.Recall(name="ai_recall")
+            ]
+        }
+    )
+
+    model_history = model.fit(
+        train_dataset, 
+        validation_data=test_dataset,
+        epochs=epoch_count,
+        callbacks=callbacks,
+        verbose=1
+    )
+
+    save_history_as_json(model_history, project_setting['model_history_path'])
+
+    # Save model as keras
+    model.save(project_setting['model_path'])
+
+    model_artifact_path = project_setting['model_artifact_path'] 
+
+    # Save model as TF SavedModel
+    # See following link. 
+    # https://www.tensorflow.org/api_docs/python/tf/keras/Model?_gl=1*145jd63*_up*MQ..*_ga*MzAxMzM3NDQyLjE3NDY5ODYwNjg.*_ga_W0YLR4190T*czE3NDY5ODYwNjckbzEkZzAkdDE3NDY5ODY0MjckajAkbDAkaDA.
+    model.export(model_artifact_path)
+
+    # Using AdamW too in fine tuning 
+    fine_tuning_pre_trained_based_model_with_warmup(
+        project_setting=project_setting,
+        model=model,
+        train_dataset=train_dataset,
+        test_dataset=test_dataset,
+        # Set loss and metrics for binary classification
+        loss={
+            "ai_prediction": "binary_crossentropy"
+        }, 
+        metrics={
+            "ai_prediction": [
+                tf.keras.metrics.BinaryAccuracy(name="ai_acc"),
+                tf.keras.metrics.Precision(name="ai_precision"),
+                tf.keras.metrics.Recall(name="ai_recall")
+            ]
+        },
+        epoch=10,
+        total_steps=total_steps, warmup_steps=warmup_steps,
+        fine_tune_suffix="fine_tune_1",
+        unfreeze_boundary_name="block6a", # freeze after block6a_expand_conv
+        new_learning_rate=1e-5
+    )
+
 def expected_mae(y_true, y_pred):
     """
     Compute MAE from expected score of softmax output
@@ -1111,6 +1291,118 @@ def expected_mae(y_true, y_pred):
 
     # Compute MAE
     return tf.reduce_mean(tf.abs(true_score - pred_score))
+
+class WarmUpCosineDecay(tf.keras.optimizers.schedules.LearningRateSchedule):
+    """
+    Custom LearningRateSchedule class
+
+    Linear Warm-up + Cosine decay for train TransformerBlock with AdamW 
+    """
+    def __init__(self, base_lr, total_steps, warmup_steps, min_lr=1e-6):
+        super().__init__()
+        self.base_lr = base_lr
+        self.total_steps = total_steps
+        self.warmup_steps = warmup_steps
+        self.min_lr = min_lr
+
+    def __call__(self, step):
+        step = tf.cast(step, tf.float32)
+
+        # Linear Warm-up
+        warmup_lr = self.base_lr * (step / self.warmup_steps)
+
+        # Cosine Decay after warm-up
+        progress = (step - self.warmup_steps) / (self.total_steps - self.warmup_steps)
+        cosine_decay = 0.5 * (1 + tf.cos(np.pi * tf.minimum(progress, 1.0)))
+        decayed = (self.base_lr - self.min_lr) * cosine_decay + self.min_lr
+
+        # Return warmup_lr if step less than warmup_steps else decayed
+        return tf.cond(step < self.warmup_steps, lambda: warmup_lr, lambda: decayed)
+
+def fine_tuning_pre_trained_based_model_with_warmup(
+        project_setting, model: Model, 
+        train_dataset, test_dataset,
+        loss, metrics, epoch, 
+        total_steps, warmup_steps,
+        fine_tune_suffix="fine_tune_0",
+        unfreeze_boundary_name="conv5_block2",
+        new_learning_rate = 1e-5):
+    """
+    Second step of fine-tuning pre-trained model. 
+
+    Use same project_setting and Model instance, loss, metrics with first step.
+
+    You can specify learning_rate. Default is 1e-5
+
+    Using AdamW + Warm-up + Cosine Decay
+    """
+
+    # Unfreeze all layers
+    model.trainable = True
+
+    # Freeze all layers before the layer contain `unfreeze_boundary_name` in name
+    set_trainable = False
+    for layer in model.layers:
+        if unfreeze_boundary_name in layer.name:
+            set_trainable = True
+
+        layer.trainable = set_trainable
+
+    checkpoint_path = os.path.join(project_setting['project_path'], f"checkpoint_{fine_tune_suffix}")
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+
+    csv_log_path = os.path.join(project_setting['project_path'], f"training_log_{fine_tune_suffix}.csv")
+
+    callbacks = [
+        # Set EarlyStopping. 
+        # Monitor loss. stop training when no improvement in 5 epochs.
+        EarlyStopping(monitor="val_loss", mode="min", patience=5, restore_best_weights=True, verbose=1), 
+
+                # Save model for each epoch
+        ModelCheckpoint(filepath=os.path.join(checkpoint_path, "{epoch:02d}-{val_loss:.2f}.keras"),
+                        monitor="val_loss", mode="min", save_weights_only=False, verbose=1),
+
+        CSVLogger(csv_log_path),
+		
+		# Terminate when loss or metric is NaN for safety
+		TerminateOnNaN()
+    ]
+
+    # Set LearningRateSchedule
+    learning_rate_schedule = WarmUpCosineDecay(
+        base_lr=new_learning_rate,
+        total_steps=total_steps,
+        warmup_steps=warmup_steps
+    )
+
+    logger.debug(f"total_steps: {total_steps}, warmup_steps: {warmup_steps}")
+
+    # Compile again with new leaning rate and unfreeze model
+    model.compile(
+        # Set Weight decay to 1e-4 to avoid overfitting
+        optimizer=tf.keras.optimizers.AdamW(learning_rate=learning_rate_schedule, weight_decay=1e-4),
+        loss=loss,
+        metrics=metrics
+    )
+
+    model_history = model.fit(
+        train_dataset,
+        validation_data=test_dataset,
+        epochs=epoch,
+        callbacks=callbacks,
+        verbose=1
+    )
+
+    # Save fine-tuned model
+    model_path = os.path.join(project_setting['project_path'], f"model-mixed_precision_{fine_tune_suffix}.keras")
+
+    model.save(model_path)
+
+    model_artifact_path = os.path.join(project_setting['project_path'], f"model-mixed_precision-TF-SavedModel_{fine_tune_suffix}")
+
+    model.export(model_artifact_path)
+
 
 def execution_example():
     """
@@ -1288,6 +1580,21 @@ def execution_example_v6():
         }
     )
 
+def execution_example_v6_1():
+    """
+    Function for experiment '6th_experiment_pre-trained_efficientnetb4_transformer_ai_classification`
+    """
+    
+    train_ai_classification_transformer_model(
+        "/data/PixivDataBookmarks", ".database/metadata_base_sample_ai_flag.sqlite3", 
+        "", "6th_experiment_pre-trained_efficientnetb4_transformer_ai_classification", None, {
+            'image_width': 380, 'image_height': 380, 'learning_rate': 0.0001,
+            'batch_size': 48, 'epoch': 30, 'data_augmentation': {
+                "zoom_range": 0.1, "rotation_range": 0.1
+            }
+        }
+    )
+
 def for_test():
 
     print(get_project_setting(
@@ -1312,4 +1619,5 @@ if __name__ == "__main__":
     # execution_example_v4()
     # # execution_example_v5()
     # fine_tuning_example_v5()
-    execution_example_v6()
+    # execution_example_v6()
+    execution_example_v6_1()
