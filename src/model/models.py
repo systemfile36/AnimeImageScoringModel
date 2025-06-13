a753a0a5569e8c4c4a624d1cccaf1997353cf025
systemfile36 <systemfile36@gmail.com>
2025-06-14 00:38:39 +0900

    feat(models.py): Add several models for experiment.
    Add functions to create CNN backbone based sanity_level predict model.
    Add functions to create CNN backbone based manual_score predict model.
    Export functions added.

--- src/model/models.py
+++ src/model/models.py
@@ -186,6 +186,365 @@ def create_cnn_manual_score_classification_model_v2(
 
     return model
 
+def create_cnn_manual_score_classification_model_v3(
+        input: Input, cnn_delegate: Callable[..., any], 
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.2,
+        num_classes: int = 8,
+        **kwargs
+) -> Model:
+    """
+    Create score classification model based given CNN backbone 
+
+    Extend MLP head version
+
+    Dataset shape must be 
+
+    ```
+    (image, 
+        {'manual_score': "float32" }
+    )
+    ```
+
+    Args:
+        input: Input layer of Model
+        cnn_delegate: Model delegate in `src/model/cnn.py`
+        **kwargs: args of `cnn_delegate`.
+    """
+
+    # Add augmentation layers when flag is true
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        image_feature = cnn_delegate(x, **kwargs)
+    else:
+        image_feature = cnn_delegate(input, **kwargs)
+
+	# Add FFN layers and Dropout
+    # Add BatchNormalization for stable
+    ffn = layers.Dense(1024, activation='gelu', name="final_ffn_1")(image_feature)
+    ffn = layers.BatchNormalization(name="final_ffn_bn_1")(ffn)
+    ffn = layers.Dropout(0.3, name="final_dropout_1")(ffn)
+
+    ffn = layers.Dense(512, activation='gelu', name="final_ffn_2")(ffn)
+    ffn = layers.BatchNormalization(name="final_ffn_bn_2")(ffn)
+    ffn = layers.Dropout(0.3, name="final_dropout_2")(ffn)
+
+    ffn = layers.Dense(256, activation='gelu', name="final_ffn_3")(ffn)
+    ffn = layers.BatchNormalization(name="final_ffn_bn_3")(ffn)
+    ffn = layers.Dropout(0.3, name="final_dropout_3")(ffn)
+
+    # Ensure output dtype to tf.float32 for mixed precision
+    score_pred = layers.Dense(num_classes, activation='softmax', name='manual_score', dtype=tf.float32)(ffn)
+    
+    # Set output name manually to avoid error
+    model = Model(inputs=input, outputs={ "manual_score": score_pred })
+
+    return model
+
+def create_cnn_manual_score_classification_model_v4(
+        input: Input, cnn_delegate: Callable[..., any], 
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.2,
+        num_classes: int = 8,
+        **kwargs
+) -> Model:
+    """
+    Create score classification model based given CNN backbone 
+
+    Extend MLP head version
+
+    Dataset shape must be 
+
+    ```
+    (image, 
+        {'manual_score': "float32" }
+    )
+    ```
+
+    Args:
+        input: Input layer of Model
+        cnn_delegate: Model delegate in `src/model/cnn.py`
+        **kwargs: args of `cnn_delegate`.
+    """
+
+    # Add augmentation layers when flag is true
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        image_feature = cnn_delegate(x, **kwargs)
+    else:
+        image_feature = cnn_delegate(input, **kwargs)
+
+	# Add FFN layers and Dropout
+    ffn = layers.Dense(512, activation='relu', name="final_ffn_1")(image_feature)
+    ffn = layers.Dropout(0.2, name="final_dropout_1")(ffn)
+    ffn = layers.Dense(256, activation='relu', name="final_ffn_2")(ffn)
+
+    # Ensure output dtype to tf.float32 for mixed precision
+    score_pred = layers.Dense(num_classes, activation='softmax', name='manual_score', dtype=tf.float32)(ffn)
+    
+    # Set output name manually to avoid error
+    model = Model(inputs=input, outputs={ "manual_score": score_pred })
+
+    return model
+
+def create_cnn_manual_score_classification_model_v5(
+        input: Input, cnn_delegate: Callable[..., any], 
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.2,
+        num_classes: int = 8,
+        **kwargs
+) -> Model:
+    """
+    Create score classification model based given CNN backbone 
+
+    Add self-attention to `create_cnn_manual_score_classification_model_v2`
+
+    `cnn_delegate` should return non-pooling feature map
+
+    Dataset shape must be 
+
+    ```
+    (image, 
+        {'manual_score': "float32" }
+    )
+    ```
+
+    Args:
+        input: Input layer of Model
+        cnn_delegate: Model delegate in `src/model/cnn.py`
+        **kwargs: args of `cnn_delegate`.
+    """
+
+    # Add augmentation layers when flag is true
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        image_feature = cnn_delegate(x, **kwargs)
+    else:
+        image_feature = cnn_delegate(input, **kwargs)
+
+    # image_feature's shape: (batch, H, W, C)
+
+    # Flatten to tokens
+    # (batch, H, W, C) -> (batch, H * W, C)
+    # auto calculate by `-1`
+    patch_tokens = layers.Reshape((-1, image_feature.shape[-1]), name="flatten_cnn_tokens")(image_feature)
+
+    # Lightweight self-attention block
+    attn = layers.MultiHeadAttention(num_heads=2, key_dim=64, name="self_attention")(patch_tokens, patch_tokens)
+    # -> shape: (batch, H * W, 128)   # 2 heads × 64 dim each = 128 output dim
+
+    attn = layers.Dense(patch_tokens.shape[-1], name="attention_projection")(attn)
+    # -> shape: (batch, C)
+    attn = layers.Add()([attn, patch_tokens])  # Residual
+    attn = layers.LayerNormalization()(attn)
+    attn = layers.Dropout(0.1)(attn)
+    # → shape: (batch, H * W, C)   # 2 heads × 64 dim each = 128 output dim
+
+    # Add FFN and GAP1D
+    ffn = layers.Dense(512, activation='relu', name="ffn_after_attention")(attn)
+    # → shape: (batch, H * W, 512)
+    ffn = layers.GlobalAveragePooling1D(name="global_avg_pool")(ffn)
+    # → shape: (batch, 512)
+
+    # Ensure output dtype to tf.float32 for mixed precision
+    score_pred = layers.Dense(num_classes, activation='softmax', name='manual_score', dtype=tf.float32)(ffn)
+    
+    # Set output name manually to avoid error
+    model = Model(inputs=input, outputs={ "manual_score": score_pred })
+
+    return model
+
+def create_cnn_manual_score_classification_model_v6(
+        input: Input, cnn_delegate: Callable[..., any], 
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.2,
+        num_classes: int = 8,
+        **kwargs
+) -> Model:
+    """
+    Create score classification model based given CNN backbone 
+
+    Add residual to `create_cnn_manual_score_classification_model_v2`
+
+    Dataset shape must be 
+
+    ```
+    (image, 
+        {'manual_score': "float32" }
+    )
+    ```
+
+    Args:
+        input: Input layer of Model
+        cnn_delegate: Model delegate in `src/model/cnn.py`
+        **kwargs: args of `cnn_delegate`.
+    """
+
+    # Add augmentation layers when flag is true
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        image_feature = cnn_delegate(x, **kwargs)
+    else:
+        image_feature = cnn_delegate(input, **kwargs)
+
+    # Projection image_feature for residual
+    residual = layers.Dense(512, name="residual_projection")(image_feature)
+
+	# Add FFN layers and Dropout
+    ffn = layers.Dense(1024, activation='relu', name="final_ffn_1")(image_feature)
+    ffn = layers.Dense(512, name="final_ffn_2")(ffn) # no activation yet
+
+    # Normalization FFN and Residual
+    ffn = layers.LayerNormalization(name="final_ffn_layer_norm")(ffn)
+    residual = layers.LayerNormalization(name="residual_layernorm")(residual)
+
+    # Redisual connection
+    ffn = layers.Add(name="residual_connection")([ffn, residual]) 
+
+    # Apply activation after residual
+    ffn = layers.Activation('relu', name="residual_activation")(ffn) 
+
+    # Apply Dropout after residual
+    ffn = layers.Dropout(0.3, name="residual_dropout")(ffn) 
+
+    # Ensure output dtype to tf.float32 for mixed precision
+    score_pred = layers.Dense(num_classes, activation='softmax', name='manual_score', dtype=tf.float32)(ffn)
+    
+    # Set output name manually to avoid error
+    model = Model(inputs=input, outputs={ "manual_score": score_pred })
+
+    return model
+
+def create_cnn_manual_score_classification_multi_task_model_v1(
+        input: Input, cnn_delegate: Callable[..., any], 
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.2,
+        num_classes: int = 8,
+        **kwargs
+) -> Model:
+    """
+    Create multi-task score classification model based given CNN backbone 
+
+    Add multi-head to `create_cnn_manual_score_classification_model_v2`
+
+    Dataset shape must be 
+
+    ```
+    (image, 
+        {
+            'manual_score': "(num_classes,), Soft-label", 
+            'color_lighting_score': "(num_classes,), Soft-label",
+            'costume_detail_score': "(num_classes,), Soft-label",
+            'proportion_score': "(num_classes,), Soft-label"
+        }
+    )
+    ```
+
+    Args:
+        input: Input layer of Model
+        cnn_delegate: Model delegate in `src/model/cnn.py`
+        **kwargs: args of `cnn_delegate`.
+    """
+
+    # Add augmentation layers when flag is true
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        image_feature = cnn_delegate(x, **kwargs)
+    else:
+        image_feature = cnn_delegate(input, **kwargs)
+
+	# Add FFN layers and Dropout
+    ffn = layers.Dense(1024, activation='relu', name="final_ffn_1")(image_feature)
+    ffn = layers.Dropout(0.5, name="final_dropout_1")(ffn)
+    ffn = layers.Dense(512, activation='relu', name="final_ffn_2")(ffn)
+    ffn = layers.Dropout(0.3, name="final_dropout_2")(ffn)
+
+    # Ensure output dtype to tf.float32 for mixed precision
+    manual_score_pred = layers.Dense(num_classes, activation='softmax', name='manual_score', dtype=tf.float32)(ffn)
+    
+    color_lighting_score_pred = layers.Dense(num_classes, activation='softmax', name='color_lighting_score', dtype=tf.float32)(ffn)
+
+    costume_detail_score_pred = layers.Dense(num_classes, activation='softmax', name='costume_detail_score', dtype=tf.float32)(ffn)
+
+    proportion_score_pred = layers.Dense(num_classes, activation='softmax', name='proportion_score', dtype=tf.float32)(ffn)
+
+
+    # Set output name manually to avoid error
+    model = Model(inputs=input, outputs={ 
+        "manual_score": manual_score_pred,
+        "color_lighting_score": color_lighting_score_pred,
+        "costume_detail_score": costume_detail_score_pred, 
+        "proportion_score": proportion_score_pred
+    })
+
+    return model
+
+def create_cnn_manual_score_regression_multi_task_model_v1(
+        input: Input, cnn_delegate: Callable[..., any], 
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.2,
+        **kwargs
+) -> Model:
+    """
+    Create multi-task score regression model based given CNN backbone 
+
+    Dataset shape must be 
+
+    ```
+    (image, 
+        {
+            'manual_score': "(num_classes,), Soft-label", 
+            'color_lighting_score': "(num_classes,), Soft-label",
+            'costume_detail_score': "(num_classes,), Soft-label",
+            'proportion_score': "(num_classes,), Soft-label"
+        }
+    )
+    ```
+
+    Args:
+        input: Input layer of Model
+        cnn_delegate: Model delegate in `src/model/cnn.py`
+        **kwargs: args of `cnn_delegate`.
+    """
+
+    # Add augmentation layers when flag is true
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        image_feature = cnn_delegate(x, **kwargs)
+    else:
+        image_feature = cnn_delegate(input, **kwargs)
+
+	# Add FFN layers and Dropout
+    # ffn = layers.Dense(1024, activation='relu', name="final_ffn_1")(image_feature)
+    # ffn = layers.Dropout(0.5, name="final_dropout_1")(ffn)
+    # ffn = layers.Dense(512, activation='relu', name="final_ffn_2")(ffn)
+    # ffn = layers.Dropout(0.3, name="final_dropout_2")(ffn)
+
+    # Ensure output dtype to tf.float32 for mixed precision
+    manual_score_pred = layers.Dense(1, activation='linear', name='manual_score', dtype=tf.float32)(image_feature)
+    
+    color_lighting_score_pred = layers.Dense(1, activation='linear', name='color_lighting_score', dtype=tf.float32)(image_feature)
+
+    costume_detail_score_pred = layers.Dense(1, activation='linear', name='costume_detail_score', dtype=tf.float32)(image_feature)
+
+    proportion_score_pred = layers.Dense(1, activation='linear', name='proportion_score', dtype=tf.float32)(image_feature)
+
+    # Set output name manually to avoid error
+    model = Model(inputs=input, outputs={ 
+        "manual_score": manual_score_pred,
+        "color_lighting_score": color_lighting_score_pred,
+        "costume_detail_score": costume_detail_score_pred, 
+        "proportion_score": proportion_score_pred
+    })
+
+    return model
+
 def create_cnn_meta_multitask_reg_model(
         input: Input, cnn_delegate: Callable[..., any], **kwargs
 ) -> Model:
@@ -614,6 +973,122 @@ def create_cnn_rating_classification_model(
     rating_pred = layers.Dense(1, activation="sigmoid", name="rating_prediction", dtype=tf.float32)(ffn)
 
     model = Model(inputs=input, outputs={ "rating_prediction": rating_pred })
+
+    return model
+
+def create_cnn_sanity_level_classification_model(
+        input: Input, cnn_delegate: Callable[..., any], 
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.2,
+        **kwargs
+) -> Model:
+    """
+    Create sanity_level classification model based given CNN backbone
+
+    Dataset shape must be 
+
+    ```
+    (image, {
+        'sanity_level': "(3,), Gaussian soft-label vector"
+        })
+    ```
+
+    Args:
+        input: Input layer of Model
+        cnn_delegate: Model delegate in `src/model/cnn.py`
+        **kwargs: args of `vit_delegate`.
+    """
+
+
+    # Add augmentation layers when flag set
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        image_feature = cnn_delegate(x, **kwargs)
+    else:
+        image_feature = cnn_delegate(input, **kwargs)
+
+    # Add FFN layer and Dropout
+    ffn = layers.Dense(512, activation='relu', name="final_ffn")(image_feature)
+    ffn = layers.Dropout(0.3, name="final_dropout")(ffn)
+
+    sanity_red = layers.Dense(4, activation="softmax", name="sanity_level", dtype=tf.float32)(ffn)
+
+    model = Model(inputs=input, outputs={ "sanity_level": sanity_red })
+
+    return model
+
+def create_cnn_transformer_sanity_level_classification_model(
+        input: Input, cnn_delegate: Callable[..., any], 
+        token_dim = 768, final_ff_dim=2048, dropout_rate=0.1, transformer_count=2,
+        augmentation: bool=True, 
+        zoom_range: None | float | tuple[float, float] = 0.15,
+        rotation_range: None | float | tuple[float, float] = 0.1,
+        **kwargs
+) -> Model:
+    """
+    Create sanity_level classification model based given CNN backbone + Transormer Block
+
+    Dataset shape must be 
+
+    ```
+    (image, {
+        'sanity_level': "(3,), Gaussian soft-label vector"
+        })
+    ```
+
+    Args:
+        input: Input layer of Model
+        cnn_delegate: Model delegate in `src/model/cnn.py`
+        **kwargs: args of `vit_delegate`.
+    """
+
+    # Add augmentation layers when flag set
+    if augmentation:
+        x = data_augmentation(input, zoom_range=zoom_range, rotation_range=rotation_range)
+        image_feature = cnn_delegate(x, **kwargs)
+    else:
+        image_feature = cnn_delegate(input, **kwargs)
+
+
+    # Flatten to tokens
+    # (batch, H, W, C) -> (batch, H * W, C)
+    # auto calculate by `-1`
+    patch_tokens = layers.Reshape((-1, image_feature.shape[-1]), name="flatten_cnn_tokens")(image_feature)
+
+    # Project token to `token_dim`
+    # Add activation
+    patch_tokens = layers.Dense(token_dim * 2, name="cnn_patch_proj_dense_1")(patch_tokens)
+    patch_tokens = layers.Activation("relu", name="cnn_patch_proj_relu")(patch_tokens)
+    
+    # (batch, N, C)
+    patch_tokens = layers.Dense(token_dim, name="cnn_patch_proj_dense_2")(patch_tokens)
+
+    # Add Positional Encoding (without CLS token)
+    token_sequence = AddPositionalOnly(
+        num_patches=patch_tokens.shape[1],
+        projection_dim=token_dim,
+        name_prefix="score"
+    )(patch_tokens)
+
+    x = token_sequence
+
+    # Stack TransformerBlock
+    for i in range(transformer_count):
+        x = TransformerBlock(
+            projection_dim=token_dim, 
+            ffn_dim=final_ff_dim,
+            num_heads=4, dropout_rate=0.1, block_index=i, name_prefix="sanity_pred"
+        )(x)
+
+    # Pooling with GAP1D
+    pooled = layers.GlobalAveragePooling1D(name="score_pooling")(x)
+    x = layers.Dense(final_ff_dim, activation='relu')(pooled)
+    x = layers.Dropout(dropout_rate)(x)
+
+    sanity_pred = layers.Dense(4, activation='softmax', dtype=tf.float32, name="sanity_level")(x)
+
+    model = Model(inputs=input, outputs={ 'sanity_level': sanity_pred })
 
     return model
 
